ca04aaf416253c819c41fe934f77497753378d5b
vhast <fedvhast@gmail.com>
2023-12-03 16:14:08 +0200

    THM Logo fixed

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const nightModeBtn = document.getElementById('nightmode-btn');
     const body = document.body;
+    const thmLogo = document.getElementById('thm-logo');
 
     // Function to toggle dark mode
     function toggleDarkMode() {
         // Toggle dark mode class on the body
         body.classList.toggle('dark-mode');
 
-        // Update styles based on dark mode status
+        // Update styles and logo based on dark mode status
         const isDarkMode = body.classList.contains('dark-mode');
         updateStyles(isDarkMode);
+        updateThmLogo(isDarkMode);
 
         // Save the user's preference in local storage
         localStorage.setItem('dark-mode', isDarkMode);
@@ -25,10 +27,20 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Initialize styles based on user's preference
+    // Function to update THM logo based on dark mode status
+    function updateThmLogo(isDarkMode) {
+        if (isDarkMode) {
+            thmLogo.src = '/img/thmlogo-dark.png';
+        } else {
+            thmLogo.src = '/img/thmlogo.png'; // Original URL for light mode
+        }
+    }
+
+    // Initialize styles and logo based on user's preference
     if (localStorage.getItem('dark-mode')) {
         body.classList.add('dark-mode');
         updateStyles(true);
+        updateThmLogo(true);
     }
 
     // Add click event listener to the nightmode-btn
